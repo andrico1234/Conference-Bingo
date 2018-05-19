9b8730159b8971b98d3c91cc90324b480a769db4
James Henshaw <james.henshaw@trint.com>
2018-05-19 13:26:26 +0200

    Center grid tiles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React from 'react';
+import Link from 'gatsby-link';
+import '@material-ui/core/CssBaseline';
 
-
-import Bingo from '../components/BingoGrid'
+import Bingo from '../components/BingoGrid';
 
 const getMockTiles = () => {
-  let mocks = []
+  let mocks = [];
   for (let i = 0; i < 12; i++) {
-    mocks.push({ id: i, text: `Tile ${i}`, active: false })
+    mocks.push({ id: i, text: `Tile ${i}`, active: false });
   }
   return mocks;
-}
+};
 
-const IndexPage = ({ data: { contentfulAuthors: { author } } }) => {
+const IndexPage = ({
+  data: {
+    contentfulAuthors: { author },
+  },
+}) => {
   return (
     <div>
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>We are {`${author[0]} and ${author[1]}`}</p>
       <Link to="/page-2/">Go to page 2</Link>
-  <div>
-    <Bingo tiles={getMockTiles()} />
-  </div>
+      <Bingo tileData={getMockTiles()} />
     </div>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const query = graphql`
   query rootQuery {
